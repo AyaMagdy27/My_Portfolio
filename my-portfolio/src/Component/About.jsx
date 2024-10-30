77a6a12061117fdb9628eff6ec12014d31bcef5b
AyaMagdy27 <ayamado1996@gmail.com>
2024-10-30 09:40:31 +0300

    motion about me section

--- my-portfolio/src/Component/About.jsx
+++ my-portfolio/src/Component/About.jsx
@@ -9,16 +9,24 @@ const About = () => {
             </h2>
             <div className="flex flex-wrap ">
                 <div className=" w-full lg:w-1/2 lg:p-8">
-                    <div className="flex items-center justify-center">
+                    <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="flex items-center justify-center">
                         <img src={about} alt="about" className="rounded-2xl shadow-lg" />
-                    </div>
+                    </motion.div>
 
                 </div>
-                <div className="w-full lg:w-1/2">
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:justify-start">
                         <p className='my-2 max-w-xl py-6 font-light tracking-tight leading-relaxed text-purple-950 text-xl'>{ABOUT_TXT} </p>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
         </div >
